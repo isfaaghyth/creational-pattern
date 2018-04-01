08b3528f81b9136a0521bd4bff9ae09c8898a6c7
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2018-04-02 00:33:55 +0700

    showing kueBasah and kueKering in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
+import builder.Kue;
+import builder.KueBuilder;
+
 public class Main {
 
     public static void main(String[] args) {
+        KueBuilder builder = new KueBuilder();
 
+        /**
+         * menampilkan semua kue basah
+         */
+        Kue kueBasah = builder.kueBasah();
+        System.out.println("Kue basah :");
+        kueBasah.tampilkanKue();
+        System.out.println("Total harga :" + kueBasah.getHarga());
+
+        /**
+         * menampilkan semua kue kering
+         */
+        Kue kueKering = builder.kueKering();
+        System.out.println("Kue kering :");
+        kueKering.tampilkanKue();
+        System.out.println("Total harga :" + kueKering.getHarga());
     }
 
 }
