4bf3914c4c9485f9df331acbd239a950a57d17d4
Agung Prayoga <agungprayoga088@gmail.com>
2018-04-02 00:14:34 +0700

    Create Pancong.java

---
+++ src/Pancong.java
@@ -0,0 +1,13 @@
+public class Pancong extends KueBasah {
+
+    @Override
+    public float price() {
+       return 5.0f;
+    }
+
+    @Override
+    public String name() {
+        return "Pancong";
+    }
+    
+}
