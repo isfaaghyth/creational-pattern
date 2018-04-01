d8c7030c8c514569efc5b3da5fde5f9f18587d0e
Agung Prayoga <agungprayoga088@gmail.com>
2018-04-02 00:14:04 +0700

    Create Nagosari.java

---
+++ src/Nagosari.java
@@ -0,0 +1,13 @@
+public class Nagosari extends KueBasah {
+
+    @Override
+    public float price() {
+        return 10.0f;
+    }
+
+    @Override
+    public String name() {
+        return "Nagosari";
+    }
+    
+}
