e17feb4a420095714d30051cd690eda2ebdc483c
Agung Prayoga <agungprayoga088@gmail.com>
2018-04-02 00:13:11 +0700

    Create Nastar.java

---
+++ src/Nastar.java
@@ -0,0 +1,13 @@
+public class Nastar extends KueKering {
+
+    @Override
+    public float price() {
+       return 25.0f;
+    }
+
+    @Override
+    public String name() {
+        return "Nastar";
+    }
+    
+}
